LineageOS/android_device_google_sunfish

Fix regen-vendor.sh
Change-Id: I33206a028a863d9849d49a6b0ed44661e03d6e0c

regen-vendor.sh
@@ -1201,6 +1201,9 @@ VENDOR_SKIP_FILES=(
     "lib/libstpreprocess.so"
 )
 
+# Initialize the helper
+setup_vendor "${ANDROID_ROOT}"
+
 generate_prop_list_from_image "${_input_image}" "${_output_file}" VENDOR_SKIP_FILES
 
 # Fixups

PixelExperience-Devices/device_google_barbet

regen-vendor.sh
@@ -1469,6 +1469,9 @@ VENDOR_SKIP_FILES=(
     "lib64/vendor.qti.hardware.sigma_miracast@1.0.so"
 )
 
+# Initialize the helper
+setup_vendor_deps "${ANDROID_ROOT}"
+
 generate_prop_list_from_image "${_input_image}" "${_output_file}" VENDOR_SKIP_FILES
 
 # Fixups